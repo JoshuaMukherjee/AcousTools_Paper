9d29e94cf6233602f5d9a2ede748326f633801f1
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2026-01-06 15:03:07 +0000

    freq

--- compare_freq.py
+++ compare_freq.py
@@ -18,9 +18,12 @@ for k in ks:
     x = wgs(p, k=k)
     xs.append(x)
 
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 10, 'font.family' : 'times',})
+
 
 Visualise(*ABC(0.005), xs, res=(300,300), link_ax=None,arrangement=(3,2) , depth=-1,
           colour_functions=[propagate_abs_normalised,propagate_abs_normalised,propagate_abs_normalised,propagate_abs_normalised,propagate_abs_normalised,propagate_abs_normalised],
         colour_function_args=[{"k":k} for k in ks],
-        clr_labels=["Normalised Pressure", "Normalised Pressure", "Normalised Pressure", "Normalised Pressure", "Normalised Pressure", "Normalised Pressure"],
-        titles=[str(f)+'kHz' for f in fs_kHz])
+        clr_labels=["Normalised\nPressure", "Normalised'\nPressure", "Normalised\nPressure", "Normalised\nPressure", "Normalised\nPressure", "Normalised\nPressure"],
+        titles=['40kHz', '80kHz', '60kHz', '320kHz', '640kHz', '1.28MHz'])
