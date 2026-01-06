3ea6485569e991ea63987614ae6b0a097d706a48
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2026-01-06 10:41:53 +0000

    freqs

---
+++ compare_freq.py
@@ -0,0 +1,26 @@
+from acoustools.Utilities import create_points, propagate_abs_normalised
+from acoustools.Solvers import wgs
+from acoustools.Constants import c_0, pi, k
+
+from acoustools.Visualiser import Visualise, ABC
+
+
+fs_kHz = [40, 80, 160, 320, 640, 1280]
+fs = [f * 1000 for f in fs_kHz]
+
+ks = [(2 * pi * f)/c_0 for f in fs]
+
+
+p = create_points(1,1,0,0,0)
+
+xs = []
+for k in ks:
+    x = wgs(p, k=k)
+    xs.append(x)
+
+
+Visualise(*ABC(0.005), xs, res=(300,300), link_ax=None,arrangement=(3,2) , depth=-1,
+          colour_functions=[propagate_abs_normalised,propagate_abs_normalised,propagate_abs_normalised,propagate_abs_normalised,propagate_abs_normalised,propagate_abs_normalised],
+        colour_function_args=[{"k":k} for k in ks],
+        clr_labels=["Normalised Pressure", "Normalised Pressure", "Normalised Pressure", "Normalised Pressure", "Normalised Pressure", "Normalised Pressure"],
+        titles=[str(f)+'kHz' for f in fs_kHz])
